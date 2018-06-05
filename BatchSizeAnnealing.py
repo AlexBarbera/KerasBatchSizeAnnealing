bd29901db9faa7585ce58ff753e3aeb3e8eb49a1
Alex Barbera <alex_barbera.m@hotmail.com>
2018-06-05 13:16:19 +0200

    Cleanup, added debug per epoch

--- BatchSizeAnnealing.py
+++ BatchSizeAnnealing.py
@@ -2,20 +2,26 @@
 
 import keras
 import numpy
+import sys
 
 class BatchSizeAnnealing():
     def __init__(self, model, callback, x, y, **kwargs):
         self.model = model
         self.args = {}
-	for (k,v) in kwargs.iteritems():
-		if k == "batch_size":
-			continue
-		elif k == "epochs":
-			self.args[k] = 1
-		else:
-			self.args[k] = v
-
+        self.verbose = False
         self.epochs = kwargs["epochs"]
+        
+        for (k,v) in kwargs.iteritems():
+            if k == "batch_size":
+                continue
+            elif k == "epochs":
+                self.args[k] = 1
+            elif k == "verbose":
+                self.verbose = True
+                self.args[k] = 0
+            else:
+                self.args[k] = v
+
 	print self.args
         self.f = callback
         self.x = x
@@ -24,9 +30,18 @@ class BatchSizeAnnealing():
     def train(self):
         output = []
         for i in xrange(self.epochs):
-		    print "Epoch %d/%d" % (i+1,self.epochs)		
-		    output.append( self.model.fit(self.x, self.y, batch_size=self.f(i), **self.args) )
-		    output[-1].epoch = [i]
+            if self.verbose:
+		        sys.stdout.write("Epoch %d/%d:\n" % (i+1, self.epochs))
+		        sys.stdout.flush()
+		        sys.stdout.write( "[%-60s] %d%%\n" % ("="*(60*(i+1)/self.epochs), (100*(i+1)/self.epochs) ))
+		        sys.stdout.flush()
+		    
+            output.append( self.model.fit(self.x, self.y, batch_size=self.f(i), **self.args) )
+            output[-1].epoch = [i]
+            
+            if self.verbose:
+                print output[-1].history
+                
         
         return self.__mergeHistory(output)
         
@@ -34,7 +49,6 @@ class BatchSizeAnnealing():
         output = keras.callbacks.History()
 	print vars(output), history
         for h in history:
-	    print vars(h)
             #soutput.epoch = output.epoch + h.epoch
             for key in vars(h):
                 if type(getattr(h,key)) == list:
